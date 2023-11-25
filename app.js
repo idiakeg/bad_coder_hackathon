ce2e2b3401980ff561b354037044aab91c9dbc27
idiakeg <idiakeg@gmail.com>
2023-11-25 21:05:47 +0100

    completed the navigation

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function app() {
-	// =============script to toggle the navigation menu open or close.
-	const navigationMenuTrigger = document.getElementById("notification_menu");
-	const navigationDropdown = document.getElementById("notification_dropdown");
-	const navigationActionableItems =
-		navigationDropdown.querySelectorAll("[role='menuitem']");
+	// =============script to toggle the notification menu open or close.
+	const notificationMenuTrigger = document.getElementById("notification_menu");
+	const notificationDropdown = document.getElementById("notification_dropdown");
+	const notificationActionableItems =
+		notificationDropdown.querySelectorAll("[role='menuitem']");
 
 	function handleMenuKeypress(e) {
 		if (e.key === "Escape") {
@@ -13,19 +13,19 @@ function app() {
 
 	function handleArrowkeypress(e, index) {
 		// check if user on the last item in the nodelist
-		const isLastItem = index === navigationActionableItems.length - 1;
+		const isLastItem = index === notificationActionableItems.length - 1;
 		const isFirstItem = index === 0;
 
-		const nextItem = navigationActionableItems.item(
+		const nextItem = notificationActionableItems.item(
 			index + 1
 		); /* recall that we have a function that autofocuses on the first node item when the dropdown is opened, so, when dropdown opens the index is 1, to move to the next index we just add 1 to the current index*/
 
-		const prevItem = navigationActionableItems.item(index - 1);
+		const prevItem = notificationActionableItems.item(index - 1);
 
 		if (e.key === "ArrowRight" || e.key === "ArrowDown") {
 			// check if user is at the last item in the node list, if yes, put focus on the first item
 			if (isLastItem) {
-				navigationActionableItems.item(0).focus();
+				notificationActionableItems.item(0).focus();
 				return;
 			}
 
@@ -36,8 +36,8 @@ function app() {
 		if (e.key === "ArrowLeft" || e.key === "ArrowUp") {
 			// check if user is at the first item in the node list, if yes, put focus on the last item
 			if (isFirstItem) {
-				navigationActionableItems
-					.item(navigationActionableItems.length - 1)
+				notificationActionableItems
+					.item(notificationActionableItems.length - 1)
 					.focus();
 				return;
 			}
@@ -49,33 +49,38 @@ function app() {
 
 	function openMenu() {
 		// opens the closed dropdown
-		navigationMenuTrigger.ariaExpanded = "true";
+		notificationMenuTrigger.ariaExpanded = "true";
 		// when the dropdown is opened, send focus to the first actionable item
-		navigationActionableItems.item(0).focus();
+		notificationActionableItems.item(0).focus();
 
 		// add an event listener on the dropdown container
-		navigationDropdown.addEventListener("keyup", handleMenuKeypress);
+		notificationDropdown.addEventListener("keyup", handleMenuKeypress);
 
 		// when the dropdown is open, add an event listener to all the actionable items
-		navigationActionableItems.forEach((item, index) => {
+		notificationActionableItems.forEach((item, index) => {
 			item.addEventListener("keyup", (e) => handleArrowkeypress(e, index));
 		});
 	}
 
 	function closeMenu() {
 		// closes the open dropdown
-		navigationMenuTrigger.ariaExpanded = "false";
+		notificationMenuTrigger.ariaExpanded = "false";
 		// when the dropdown closes, send focus back to the trigger element
-		navigationMenuTrigger.focus();
+		notificationMenuTrigger.focus();
 	}
 
 	function toggleMenu() {
+		// check if the dcdropdown menu is open, if it is. close it
+		if (dcTrigger.ariaExpanded === "true") {
+			dcToggle();
+		}
+
 		// check if the menu is open or not
 		const isExpanded =
-			navigationMenuTrigger.attributes["aria-expanded"].value === "true";
+			notificationMenuTrigger.attributes["aria-expanded"].value === "true";
 
 		// toggle the visiblity of the dropdown
-		navigationDropdown.classList.toggle("display_block");
+		notificationDropdown.classList.toggle("display_block");
 
 		if (isExpanded) {
 			// if the dropdown was open when the button was clicked, close the dropdown
@@ -86,7 +91,111 @@ function app() {
 		}
 	}
 
-	navigationMenuTrigger.addEventListener("click", toggleMenu);
+	notificationMenuTrigger.addEventListener("click", toggleMenu);
+
+	// =============script to toggle the davi_collection menu open or close.
+	// get the required elements
+	const dcTrigger = document.getElementById("dc");
+	const dcDropdownContainer = document.getElementById("dc_dropdown");
+	const dcDropdownActionableItems =
+		dcDropdownContainer.querySelectorAll('[role="menuitem"]');
+
+	function handleDcMenuKeyup(e) {
+		if (e.key === "Escape") {
+			dcToggle();
+		}
+	}
+
+	function hanleDcArrowKeyup(e, index) {
+		// check if user on the last item in the nodelist
+		const isLastItem = index === dcDropdownActionableItems.length - 1;
+		const isFirstItem = index === 0;
+
+		const nextItem = dcDropdownActionableItems.item(
+			index + 1
+		); /* recall that we have a function that autofocuses on the first node item when the dropdown is opened, so, when dropdown opens the index is 1, to move to the next index we just add 1 to the current index*/
+
+		const prevItem = dcDropdownActionableItems.item(index - 1);
+
+		if (e.key === "ArrowRight" || e.key === "ArrowDown") {
+			// check if user is at the last item in the node list, if yes, put focus on the first item
+			if (isLastItem) {
+				dcDropdownActionableItems.item(0).focus();
+				return;
+			}
+
+			// put focus on the nextItem
+			nextItem.focus();
+		}
+
+		if (e.key === "ArrowLeft" || e.key === "ArrowUp") {
+			// check if user is at the first item in the node list, if yes, put focus on the last item
+			if (isFirstItem) {
+				dcDropdownActionableItems
+					.item(dcDropdownActionableItems.length - 1)
+					.focus();
+				return;
+			}
+
+			// put focus on the prevItem
+			prevItem.focus();
+		}
+	}
+
+	function closeDcMenu() {
+		// set the value of the aria expanded
+		dcTrigger.ariaExpanded = "false";
+		// return focus to the trigger element
+		dcTrigger.focus();
+	}
+
+	function openDcMenu() {
+		// set the value of the aria expanded
+		dcTrigger.ariaExpanded = "true";
+		// return focus to the first element
+		dcDropdownActionableItems.item(0).focus();
+
+		// when user presses escape, close the dropdown
+		dcDropdownContainer.addEventListener("keyup", handleDcMenuKeyup);
+
+		// when the dropdown is open, add an event listener to all the actionable items to listen for a keypress/keyup event
+		dcDropdownActionableItems.forEach((item, index) => {
+			item.addEventListener("keyup", (e) => {
+				hanleDcArrowKeyup(e, index);
+			});
+		});
+	}
+
+	function dcToggle() {
+		// check the aria-expanded of the notification, if true, call the toggle menu
+		if (notificationMenuTrigger.ariaExpanded === "true") {
+			toggleMenu();
+		}
+
+		// onclick of the button, toggle the class
+		dcDropdownContainer.classList.toggle("display_block");
+
+		// check if the aria expanded is open or not
+		const isAriaExpanded =
+			dcTrigger.attributes["aria-expanded"].value === "true";
+
+		// if the aria expanded id true, call the function to close the menu, else do the opposite
+		if (isAriaExpanded) {
+			closeDcMenu();
+		} else {
+			openDcMenu();
+		}
+	}
+
+	dcTrigger.addEventListener("click", dcToggle);
 }
 
 app();
+
+// const flag = dcTrigger.ariaExpanded === "true";
+// if (flag) {
+// 	dcTrigger.ariaExpanded = "false";
+// } else {
+// 	dcTrigger.ariaExpanded = "true";
+// }
+// console.log(dcTrigger.ariaExpanded);
